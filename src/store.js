33383d153d0fc899eb3b71e52d6dd401e55121e3
carl2087 <carlelli5@hotmail.co.uk>
2024-02-04 15:50:11 +0000

    pet api service added for get pet, get breed and search

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adoptedPet from "./adoptedPetSlice";
 import searchParams from "./searchParamsSlice";
+import { petApi } from "./petApiService";
 
 const store = configureStore({
   reducer: {
     adoptedPet,
     searchParams,
+    [petApi.reducerPath]: petApi.reducer,
   },
 });
 
